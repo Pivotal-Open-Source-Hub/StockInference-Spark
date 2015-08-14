2c19463a4ddc1ad17fe04470449086c1d36c26c7
Frederico Melo <fmelo@frederimelosmbp.attlocal.net>
2015-08-14 11:15:53 -0700

    working on the R script

--- streaming/tech_indicators.R
+++ streaming/tech_indicators.R
@@ -14,10 +14,13 @@ while(TRUE) {
   streamRow <- fromJSON(line)
   
 
-  historical <- getURL(paste0('http://geode-server:8080/gemfire-api/v1/queries/adhoc?q=SELECT%20DISTINCT%20*%20FROM%20/Stocks%20s%20ORDER%20BY%20entryTimestam%20desc%20LIMIT%20100'))
+  historical <- getURL(paste0('http://geode-server:8080/gemfire-api/v1/queries/adhoc?q=SELECT%20DISTINCT%20*%20FROM%20/Stocks%20s%20WHERE%20s.entryTimestamp%20<%20',streamRow$entryTimestamp,'l%20ORDER%20BY%20s.entryTimestamp%20desc%20LIMIT%20100'))
 
+  
   historicalSet <- fromJSON(historical)
-  historicalSet <- historicalSet[order(historicalSet$entryTimestamp),]
+  
+  # reverse order
+  historicalSet <-historicalSet[order(historicalSet$entryTimestamp),]
 
   dataset <- subset(historicalSet, select = c("DaysHigh", "DaysLow", "LastTradePriceOnly")) 
   names(dataset) <- c("High","Low","Close")
@@ -46,27 +49,29 @@ while(TRUE) {
   names(inputs) <- c("close", "ema", "ema_diff", "rsi", "high_diff", "low_diff")
  
   inputs <- inputs[-1:-35,]
+  historicalSet <- historicalSet[-1:-35,]
   dataset <- dataset[-1:-35,]
   ema_lag <- ema_lag[-1:-35]
 
-  # Update last line, with the real future_ema value
-  updated_rows <- data.frame(historicalSet$entryTimestamp[nrow(historicalSet)-1], inputs$ema[nrow(inputs)-1], inputs$ema[nrow(inputs)],inputs$close[nrow(inputs)-1]);
-  names(updated_rows) <- c("entryTimestamp", "ema", "future_ema", "close")
+  if (nrow(inputs)>0){
 
-  #Add new row to the end of historical dataset for computing technical indicators.
-  temprow <- matrix(c(rep.int(NA,length(updated_rows))),nrow=1,ncol=length(updated_rows))
-  newrow <- data.frame(temprow)
-  colnames(newrow) <- colnames(updated_rows)
-  updated_rows <- rbind(updated_rows,newrow)
+  	# Update last line, with the real future_ema value
+  	updated_rows <- data.frame(historicalSet$entryTimestamp[nrow(historicalSet)], inputs$ema[nrow(inputs)-1], inputs$ema[nrow(inputs)],inputs$close[nrow(inputs)-1]);
+  	names(updated_rows) <- c("entryTimestamp", "ema", "future_ema", "close")
 
 
+  	#Add new row to the end of historical dataset for computing technical indicators.
+  	temprow <- matrix(c(rep.int(NA,length(updated_rows))),nrow=1,ncol=length(updated_rows))
+  	newrow <- data.frame(temprow)
+  	colnames(newrow) <- colnames(updated_rows)
+  	updated_rows <- rbind(updated_rows,newrow)
 
-  # create the new line, with values coming from the std input and calculated indicators
-  updated_rows[nrow(updated_rows),] <- data.frame(streamRow$entryTimestamp, inputs$ema[nrow(inputs)], 0, inputs$close[nrow(inputs)]);
+  	# create the new line, with values coming from the std input and calculated indicators
+  	updated_rows[nrow(updated_rows),] <- data.frame(streamRow$entryTimestamp, inputs$ema[nrow(inputs)], 0, inputs$close[nrow(inputs)]);
 
-  updated_rows <- toJSON(updated_rows);
-  cat (updated_rows)
-
+  	updated_rows <- toJSON(updated_rows);
+  	cat (updated_rows)
+  }
 
   write('\r\n',stdout())
 }
