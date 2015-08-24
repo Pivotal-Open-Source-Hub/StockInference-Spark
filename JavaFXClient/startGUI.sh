87b56f54cf25891d8261e07de00ed11b4d953ffd
markito <william.markito@gmail.com>
2015-08-23 21:32:33 -0700

    Fixing startup script for fx

--- JavaFXClient/startGUI.sh
+++ JavaFXClient/startGUI.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-BOOT2DOCKER_IP=`boot2docker ip`
-echo boot2docker ip: $BOOT2DOCKER_IP
-
-sed "s/BOOT2DOCKER_IP/${BOOT2DOCKER_IP}/g" src/main/resources/client.xml.template > src/main/resources/client.xml
-
 ./gradlew run
